58465c6a06d5d6d570c24092680fac5233288b44
DanielGalvis-Dev <djgz2004@gmail.com>
2024-10-26 11:25:54 -0500

    advances

--- projects/StarWars-API/src/app/settings/appsettings.ts
+++ projects/StarWars-API/src/app/settings/appsettings.ts
@@ -1,9 +1,9 @@
 // appSetting.ts
 export let apiUrl = {
-  films: 'https://swapi.dev/api/films/',
   people: 'https://swapi.dev/api/people/',
   planets: 'https://swapi.dev/api/planets/',
+  films: 'https://swapi.dev/api/films/',
   species: 'https://swapi.dev/api/species/',
-  starships: 'https://swapi.dev/api/starships/',
   vehicles: 'https://swapi.dev/api/vehicles/',
+  starships: 'https://swapi.dev/api/starships/',
 };
--- projects/StarWars-API/src/app/views/inicio/inicio.component.ts
+++ projects/StarWars-API/src/app/views/inicio/inicio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatListModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })
